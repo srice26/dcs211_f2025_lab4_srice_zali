955f4d04dda185b42f11d23702805cc2c2520698
Zain Ali <zainalimalikk@gmail.com>
2025-10-30 16:22:02 -0400

    complete

--- dcs211_lab4.py
+++ dcs211_lab4.py
@@ -1,5 +1,6 @@
 import  pandas as pd
-#import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import requests
 from prettytable import PrettyTable
 
 df = pd.read_csv("county_economic_status_2024.csv", skiprows = 4, 
@@ -106,12 +107,110 @@ def printTableBy(dataframe: pd, field: str, how_many: int, title: str) -> None:
     print(table_data)
 
 
-def main():
-    #top_states()
-    #bottom_states()
-    #county_poverty_rates()
+def createByStateBarPlot(df: pd.DataFrame, field: str, filename: str, title: str, ylabel: str) -> None:
+    '''
+    prints useful messages to provide data statistics
+    args:
+        - dataframe: pd, overall pandas dataframe
+        - field: str, what is being measured
+        - filename: str, name of the file that is being created
+        - title: str, title of table
+        -ylabel: str, title of the y-axis of the graph
+    returns = none
+    '''
+
+    us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "Virgin Islands, U.S.": "VI",
+    }
+    
+    abbrev_to_us_state = dict(map(reversed, us_state_to_abbrev.items()))
+
+    grouped_states = df.groupby("state")[field].mean().sort_values(ascending=True)
+
+    abbrev_list = []
+    for state in grouped_states.index:
+        if state in us_state_to_abbrev:
+            abbrev_list.append(us_state_to_abbrev[state])
+    grouped_states.index = abbrev_list
+
+    plt.figure(figsize=(12, 6))
+    plt.bar(grouped_states.index, grouped_states.values)
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.savefig(filename)
+    plt.close()
+
+def main() -> None:
+    '''
+    used to execute each of the functions above
+    args = none
+    returns = none
+    '''
+    top_states()
+    bottom_states()
+    county_poverty_rates()
     printTableBy(df, 'Poverty Rate', 10, "counties by poverty rate")
     printTableBy(df, 'Market Income', 10, "counties by pci")
     printTableBy(df, 'Average Unemployment Rate', 10, "counties by Average Unemployment Rate")
-
+    createByStateBarPlot(df, 'Poverty Rate', "pov_rate.png", "States By Poverty Rate", "Poverty Rate")
+    createByStateBarPlot(df, 'Average Unemployment Rate', "unemp_rate.png", "States By Unemployment Rate", "Employment Rate")
+    
 main()
